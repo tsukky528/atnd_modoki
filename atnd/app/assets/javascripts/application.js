c006fd9a6292ee79316eca084ee8001ed68f0f1b
tsukky528 <e.tsukiyama@aktsk.jp>
2014-02-12 22:14:30 +0900

    change event index

--- atnd/app/assets/javascripts/application.js
+++ atnd/app/assets/javascripts/application.js
@@ -15,3 +15,10 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-datetimepicker
+$(function() {
+    $('#datetimepicker2').datetimepicker({
+      language: 'en',
+      pick12HourFormat: true
+    });
+  });
--- atnd/app/uploaders/image_uploader.rb
+++ atnd/app/uploaders/image_uploader.rb
@@ -40,7 +40,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   process :scale => [50, 50]
   # end
   version :thumb do
-    process :resize_to_limit => [200, 200]
+    process :resize_to_limit => [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
